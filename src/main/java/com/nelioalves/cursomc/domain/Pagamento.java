ad29ece17377bff536900c34050627f2a6fc74f4
Samara Teles <samara11_09@hotmail.com>
2021-02-14 17:08:11 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/nelioalves/cursomc/domain/Pagamento.java
+++ src/main/java/com/nelioalves/cursomc/domain/Pagamento.java
@@ -10,7 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
@@ -23,7 +23,7 @@ private static final long serialVersionUID = 1L;
 	
 	private Integer estado;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@JoinColumn(name="pedido_id")
 	@OneToOne
 	@MapsId

93f8987138bbe5ad6a0d77550b64be18d850685d
Samara Teles <samara11_09@hotmail.com>
2021-02-19 00:49:08 -0300

    validacao sintatica com Bean validation

--- src/main/java/com/nelioalves/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/nelioalves/cursomc/dto/CategoriaDTO.java
@@ -2,12 +2,19 @@ package com.nelioalves.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
 import com.nelioalves.cursomc.domain.Categoria;
+
+import org.hibernate.validator.constraints.Length;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ter entre 5 e 80 caracteres")
 	private String nome;
 	
 	public CategoriaDTO () {

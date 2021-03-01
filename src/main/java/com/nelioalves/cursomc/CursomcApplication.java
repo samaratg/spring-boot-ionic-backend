c86c1a00c9a5bd1961198d98d10644e694495786
samaratg <samara11_09@hotmail.com>
2021-03-01 00:13:01 -0300

    Enviando imagem via endpoint

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,18 +1,13 @@
 package com.nelioalves.cursomc;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.nelioalves.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-	@Autowired
-	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -20,7 +15,6 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {	
-		s3Service.uploadFile("C:\\Users\\samar\\Pictures\\paisagem.jpg");
 	}	
 		
 }
